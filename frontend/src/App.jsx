a244f846953e4d8e783184b69c850271fd651e63
Nagavalli14 <nagavalliuniverse@gmail.com>
2026-09-05 13:29:25 +0530

    Connect frontend to deployed backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,7 +29,7 @@ import {
 
 import "./App.css";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://marketpulse-64o8.onrender.com/";
 
 function App() {
   // =========================================================
